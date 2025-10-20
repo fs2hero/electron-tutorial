ad6750341b8bb2291d874827a4720898aca0876b
fs2hero <fs2hero@gmail.com>
2025-10-21 00:33:34 +0800

    Hello Electron

---
+++ main.js
@@ -0,0 +1,29 @@
+// console.log('Hello from Electron 👋')
+const { app, BrowserWindow, ipcMain } = require('electron')
+const path = require('node:path')
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
+  })
+
+  win.loadFile('index.html')
+}
+
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+
+  ipcMain.handle('ping', () => 'pong pang qiang')
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit()
+})
